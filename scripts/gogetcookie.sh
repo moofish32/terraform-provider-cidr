4e38c91da0b69430374bcadc570eda88d3dcb192
Mike Cowgill <moofish32@gmail.com>
2018-05-16 17:40:39 -0700

    Move to dep from govendor (#3)
    * changes to travis
    
    * moving go dep
    
    * updating git cookie
    
    * fixing test for wasted subnet calcs

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+set +o history
 touch ~/.gitcookies
 chmod 0600 ~/.gitcookies
 
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-moofish32.gmail.com=1/ziSvpbUBIqftGXePPhyKmpqoFrlEyW7kVLFCX_Op-GEovUPxec4eJOlQ_OVQnYi1
+go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-moofish32.gmail.com=1/ziSvpbUBIqftGXePPhyKmpqoFrlEyW7kVLFCX_Op-GEovUPxec4eJOlQ_OVQnYi1
 __END__
+set -o history
